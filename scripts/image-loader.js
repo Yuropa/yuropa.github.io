f26b94f4ab214b85d924831c4dceb5dd98c5316e
Josh Ford <jford@integra.net>
2019-02-05 22:34:30 -0800

    Move to using JPEG instead of PNG. Not really a good idea in the first place

--- scripts/image-loader.js
+++ scripts/image-loader.js
@@ -6,13 +6,13 @@ function loadContentForElement(elm) {
         $(elm).find('.description').click(function() {
             window.open('image-viewer.html?' + loc, '_self');
         });
-        
+
         function loadImage(idx) {
             if (idx < 1) {
                 return;
             }
-            
-            var path = loc + '/' + idx + '.png';
+
+            var path = loc + '/' + idx + '.jpg';
             $downloadImage = $('<img/>');
             $downloadImage.on('load', function() {
                 $(elm).css('background-image', 'url(' + $(this).attr('src') + ')');
@@ -25,23 +25,23 @@ function loadContentForElement(elm) {
                 if ($(elm).hasClass('fill-image')) {
                     $(elm).css('padding-bottom', (100.0 * this.height / this.width) + '%');
                 }
-                
+
                 var position = $(elm).attr('image-pos');
                 if (position == undefined || position.length < 1) {
                     position = 'center';
                 }
-                
+
                 $(elm).css('background-position', position);
-                
-                
+
+
                 if (this.width < $(elm).width() || this.height < $(elm).height()) {
                     loadImage(idx - 1);
                 }
             });
-            
+
             $downloadImage.attr('src', path);
         }
-        
+
         loadImage(desc.levels);
     });
 }
@@ -52,16 +52,16 @@ function loadSectionContentForElement(elm) {
     $.getJSON(loc, function(desc) {
         if (desc.title != undefined) {
             var titleElement = $('<div class="content-title">' + desc.title + '</div>');
-            
+
             var startColor = desc['color-start'];
             if (startColor != undefined) {
                 titleElement.css('color', desc.color);
-                
+
                 var endColor = desc['color-end'];
                 if (endColor == undefined) {
                     endColor = startColor;
                 }
-                
+
                 titleElement.addClass('content-title-background');
                 titleElement.css({
                     'background' : 'linear-gradient(70deg, ' + startColor + ',' + endColor + ')',
@@ -71,31 +71,31 @@ function loadSectionContentForElement(elm) {
                     'color' : 'transparent'
                 });
             }
-            
+
             var container = $('<div class="content-title-container"></div>');
             container.append(titleElement);
             $elm.append(container);
         }
-        
+
         var content = $('<div class="content-container"></div>');
-        
+
         var items = desc.items;
         var arrayLength = items.length;
         for (var i = 0; i < arrayLength; i++) {
             var item = items[i];
-            
+
             var elementContent;
             if (item.photo != undefined) {
                 elementContent = '<div class="image-loader fill-image" image-src="' + item.photo + '"image-pos="top" ></div>';
             } else if (item.text != undefined) {
                 elementContent = '<p>' + item.text + '</p>';
-                
+
             }
-            
+
             if (elementContent != undefined) {
                 var itemElement = $('<div class="content-item"></div>');
                 itemElement.append(elementContent);
-                
+
                 var startColor = item['color-start'];
                 if (startColor != undefined) {
                     itemElement.css('color', 'white');
@@ -104,27 +104,27 @@ function loadSectionContentForElement(elm) {
                     if (endColor == undefined) {
                         endColor = startColor;
                     }
-                    
+
                     itemElement.css({
                         'background' : 'linear-gradient(70deg, ' + startColor + ',' + endColor + ')',
                     });
                 }
-                
+
                 if (item.link != undefined) {
                     itemElement.addClass('content-item-link');
-                    
-                    
+
+
                     itemElement.click(function(item) {
                         return function() {
                             window.open(item.link, '_self');
                         }
                     } (item));
                 }
-                
+
                 content.append(itemElement);
             }
         }
-        
+
         $elm.append(content);
         $elm.find('.image-loader').each(function() {
             loadContentForElement(this);
@@ -136,11 +136,11 @@ $(document).ready(function() {
     $('.image-loader').each(function() {
         loadContentForElement(this);
     });
-        
+
     $('.content-loader').each(function() {
-        loadSectionContentForElement(this); 
+        loadSectionContentForElement(this);
     });
-    
+
     setTimeout(function() {
         $('.title-container .subtitle').addClass('filled');
     }, 1500);
